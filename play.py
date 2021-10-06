f884783482c9c2442227b611cf8f1d24ff43fe56
Nav L <nlingala3@gatech.edu>
2021-10-06 01:27:03 -0400

    buffer loader

---
+++ play.py
@@ -0,0 +1,30 @@
+import os
+import socket
+import threading
+
+SERVER_DATA_PATH = r'C:\Users\navne\Documents\3872_TA\files'
+
+def buffer_load(buf):
+    files = os.listdir(SERVER_DATA_PATH)
+    for f in files:
+        rob, cue_time= f.split("_")
+        cue_time, type = cue_time.split(".")
+        cue_time = int(cue_time)
+        tup = (f, cue_time)
+        if rob in buf:
+            if not isinstance(buf[rob], list):
+                buf[rob] = [buf[rob]]
+            buf[rob].append(tup)
+            buf[rob].sort(key=lambda tup: tup[1])
+        else:
+            buf[rob] = tup
+
+
+def main():
+    buffer = {}
+    print(buffer)
+    buffer_load(buffer)
+    print(buffer)
+
+if __name__ == "__main__":
+    main()
